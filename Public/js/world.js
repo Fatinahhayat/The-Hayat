058f19c8834c14fc685cd79e85f28e88911da561
Fatinah Hayat <fatinah.hayat@gmail.com>
2024-01-19 11:14:13 +0000

    commit

---
+++ Public/js/world.js
@@ -0,0 +1,30 @@
+
+// $(document).ready(function(){
+//     // $(".hide").hide();
+//  // $("markerColombia").click(function(){
+//    // $("colombiaText").toggle();
+//    $('colombiaText')
+//   });
+// 
+// console.log()
+//console.log(1234);
+//function myFunction(event) {
+
+// var element =document.getElementById("colombiaText");
+// element.classList.remove("hide");
+//console.log("hello world");
+//var element =document.getElementById("colombiaText");
+// console.log(event);
+//element.classList.remove("hide");
+
+//} var element =document.getElementById("colombiaEllipse"); 
+//element.addEventListener("click", myFunction);
+
+
+let showBox = document.getElementById('colombiaEllipse');
+
+function showBox() {
+    if (colombiaEllipse.style.display = "none"){
+        colombiaText.style.display = "block"
+    }
+}
